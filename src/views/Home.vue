e477777df88fe99b312de92fb02e5b22ec8b80a2
vndrd <ivangddzzz@gmail.com>
2020-06-04 22:53:01 -0400

    estructura básica

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <input type="text" v-model="name">
+      <br>
+      <button @click="agregar"> Agregar </button>
+      <br>
+    </form>
+    <br>
+    <table style="border:1px solid;">
+      <thead>
+        <tr>
+          <th v-for="club in allClubs" :key="club"> {{club}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="club in allClubs" :key="club">
+          <td>{{club}}</td>
+          <td v-for="(club,item) in allClubs.length-1" :key="item">qwe</td>
+
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      allClubs : ['Bolivar','The Strongest','Always Ready','Wilstermann'],
+      name: ''
+    }
+  },
+  methods: {
+    agregar(){
+      [this.allClubs] = [...this.allClubs, name]
+      this.name=''
+    },
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
